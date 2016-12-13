d03b12031c4cd2730925ef8daef037e035da3e28
Caleb L. Power <cpower@uco.edu>
2016-12-12 21:59:23 -0600

    added Maven and Spark

--- src/net/modulaire/npilookup/Core.java
+++ src/net/modulaire/npilookup/Core.java
@@ -2,15 +2,22 @@ package net.modulaire.npilookup;
 
 import net.modulaire.npilookup.connector.ConnectorException;
 import net.modulaire.npilookup.connector.DBConnector;
+import net.modulaire.npilookup.userinterface.FrameworkDriver;
 
 public class Core {
 
   private static final int CLEAN_EXIT = 0;
   private static final int DIRTY_EXIT = 1;
   
-  private static DBConnector dbc;
+  //private static DBConnector dbc;
+  private static FrameworkDriver fd;
   
   public static void main(String[] args) {
+    
+    System.out.println("[CORE] Hello, world!");
+    fd = new FrameworkDriver();
+    
+    /* THIS CODE IS PART OF THE DEVELOPMENT FOR DBConnector.
     
     if(args.length < 3) { //make sure that we are getting something that resembles valid data
       System.out.println("Usage: java -jar <executable.jar> <server_location> <username> <password>");
@@ -32,6 +39,8 @@ public class Core {
       if(e.getCause() != null) System.out.println("Note that the exception was caused by " + e.getCause().getClass().getSimpleName());
       System.exit(DIRTY_EXIT);
     }
+    
+    */
     
     
   }
---
+++ src/net/modulaire/npilookup/userinterface/FrameworkDriver.java
@@ -0,0 +1,9 @@
+package net.modulaire.npilookup.userinterface;
+
+public class FrameworkDriver {
+
+  public FrameworkDriver() {
+    System.out.println("[FD] The Framework Driver has been created.");
+  }
+  
+}
