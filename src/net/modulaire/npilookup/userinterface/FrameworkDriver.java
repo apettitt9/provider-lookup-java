7ecf1b1f1be35ba48713d34eff1be38f6e33a647
Caleb L. Power <cpower@uco.edu>
2016-12-21 18:56:35 -0600

    cleaned up some framework code

--- src/net/modulaire/npilookup/userinterface/FrameworkDriver.java
+++ src/net/modulaire/npilookup/userinterface/FrameworkDriver.java
@@ -6,14 +6,19 @@ import spark.Route;
 import spark.Spark;
 
 public class FrameworkDriver {
+  
+  public FrameworkDriver() {
+    Spark.get("/", (req, res) -> "Hello <strong>world</strong>");
+  }
 
+  /*
   public FrameworkDriver() {
     System.out.println("[FD] The Framework Driver has been created.");
-        /*
+        
             Request something from the database using the Get method
             this will trigger the get Route
             the user's query will go into the Request variable and the result of the query into the Response variable
-         */
+         
 
     Spark.before((request, response) -> {
       boolean validRequest = false;
@@ -23,23 +28,24 @@ public class FrameworkDriver {
         return 0;
       }
     });
-        /*
+        
             The above is a before request filter
             Use it to check the validity of the request
             If the request isn't valid, it will stop the request from going through
-         */
+         
 
-    get("/", (request, response) -> {
-            /*
+    Spark.get("/", (request, response) -> {
+            
                 call the database here
                 the result should go into the response variable
                 most of the program logic can go here
-             */
+             
       return response.body();
       //don't actually do this, it's just a filler. Probably refine it to display in some nice way
     });
 
 
   }
+*/
 
 }
